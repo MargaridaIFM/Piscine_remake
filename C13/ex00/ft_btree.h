b453e693c2de3fceeca11516797411d50c0f8d70
Margarida Matias <margarida.matiass@gmail.com>
2024-10-11 16:05:24 +0100

    ex00

---
+++ C13/ex00/ft_btree.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfrancis <mfrancis@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 13:56:20 by mfrancis          #+#    #+#             */
+/*   Updated: 2024/10/11 14:07:57 by mfrancis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+typedef struct s_btree
+{
+struct s_btree *left;
+struct s_btree *right;
+void 			*item;
+} t_btree;
+
+#endif
